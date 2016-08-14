6675e342453c6709a2e7ac6aa20f71e6e045e3f3
omen810 <8561073@qq.com>
2016-08-14 12:39:47 +0800

    能够监测到网络状态的变化

--- app/src/main/java/com/omen/broadcasttest01/MainActivity.java
+++ app/src/main/java/com/omen/broadcasttest01/MainActivity.java
@@ -1,13 +1,33 @@
 package com.omen.broadcasttest01;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter intentFilter;
+    private NetworkChangeReceiver networkChangeReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intentFilter=new IntentF    1ilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver=new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver, intentFilter);
+    }
+
+    class NetworkChangeReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "网络状态发生变化！", Toast.LENGTH_LONG).show();
+        }
     }
 }
