3d777fa9db30c387cd3393d6d15778c67f04a97a
omen810 <8561073@qq.com>
2016-08-18 14:59:59 +0800

    自定义广播之标准广播

---
+++ app/src/main/java/com/omen/broadcasttest01/MyBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package com.omen.broadcasttest01;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2016/8/18.
+ */
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "收到来自MyBroadcastReceiver的广播", Toast.LENGTH_SHORT).show();
+    }
+}
